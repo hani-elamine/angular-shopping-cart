681e606dd9ff16869984eb01732a350436fabe39
ahmadchaer <ahmad.shaer@outlook.com>
2020-07-28 18:37:18 +0400

    Add unit to products

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -1,7 +1,7 @@
 import { Product } from './product';
 
 export const PRODUCTS: Product[] = [
-  { id: 1, name: 'Potatoes', price: 5 },
-  { id: 2, name: 'Carrots', price: 4},
-  { id: 3, name: 'Onions', price: 2}
+  { id: 1, name: 'Potatoes', unit: 1, price: 5 },
+  { id: 2, name: 'Carrots', unit: 1, price: 4},
+  { id: 3, name: 'Onions', unit: 1, price: 2}
 ];
