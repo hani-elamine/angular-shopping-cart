c9ec1c5915271026f9ac4ba4298b6d0f1048b2c6
ahmadchaer <ahmad.shaer@outlook.com>
2020-07-28 17:40:09 +0400

    Add mock data

---
+++ src/app/mock-products.ts
@@ -0,0 +1,7 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  { id: 1, name: 'Potatoes', price: 5 },
+  { id: 2, name: 'Carrots', price: 4},
+  { id: 3, name: 'Onions', price: 2}
+];
