2931ff8db4e9c9b338a05d32f55d2ce7238e6d27
ahmadchaer <ahmad.shaer@outlook.com>
2020-07-29 12:03:17 +0400

    Add routes and nav router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsListComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
